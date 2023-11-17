c7148cd86256acb4cee9a7ae54312728ec76c6d6
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2023-11-17 13:54:31 +0200

    adding the control-flow to check whether the fileds are empty

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+
 import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -14,13 +15,16 @@ const uri = 'mongodb://127.0.0.1:27017/WikiDB';
 
 // Methods
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/public', express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs');
 // Connect to mongoose
 mongoose.connect(uri);
 
-// Schema/Model
+// Schema/Model:-
 const Articles = mongoose.model('Articles', {
     title: String,
     content: String
@@ -29,19 +33,52 @@ const Articles = mongoose.model('Articles', {
 // will return a json object ==> Which is the Get request
 // console.log(await Articles.find().exec());
 
-
+// GET Request:
 app.get('/articles', async (req, res) => {
-
-
-    if ((await Articles.find().exec()).length == 0) { 
-        console.log("No data to show or there's an error occured");
-    }else{
-        
-    res.send(await Articles.find().exec());
+    if ((await Articles.find().exec()).length == 0) {
+        res.send("No data to show or there's an error occured");
+    } else {
+        res.sendFile(__dirname + '/index.html');
     }
 
-    res.send('Hello World');
 });
+
+// POST Request:
+app.post('/articles', async (req, res) => {
+    try {
+        const theTitle = req.body.title;
+        const theCont = req.body.content;
+
+        if (postMethodAsSave(theTitle, theCont)) {
+            // => True
+            // Create a new article using the Mongoose model
+            const newArticle = new Articles({
+                title: theTitle,
+                content: theCont
+            });
+            await newArticle.save();
+            res.status(200).send('Article added successfully');
+
+        } else {
+            res.status(500).send('Error processing the request');
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+function postMethodAsSave(title, content) {
+
+    if (!title || !content || title.length === 0 || content.length === 0) {
+        console.log("Values are null");
+        return false;
+    } else {
+        console.log("Values aren't null");
+        return true;
+    }
+}
+
 
 app.listen(port, () => {
     console.log('app started listening');
